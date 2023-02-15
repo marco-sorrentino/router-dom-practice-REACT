1152847929eea3728d2510474e78a1d88d0f0b53
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-02-15 22:38:00 +0100

    refactoring from Class to Function

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,56 +1,62 @@
-import { Component } from "react";
 import { Card, Button, Container, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 
-class Products extends Component {
-  state = {
+const Products = () => {
+  /*   state = {
     products: [],
-  };
+  }; */
 
-  componentDidMount = () => {
+  const [products, setProducts] = useState([]);
+
+  /*   componentDidMount = () => {
     this.getData();
   };
+ */
 
-  getData = async () => {
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
     try {
       const res = await fetch("https://dummyjson.com/products");
       if (res.ok) {
         const data = await res.json();
         const prodotti = data.products;
         console.log(prodotti);
-        this.setState({
+        /*         this.setState({
           products: prodotti,
-        });
+        }); */
+        setProducts(prodotti);
       }
     } catch (error) {
       console.log(error);
     }
   };
 
-  render() {
-    return (
-      <Container className="mt-4">
-        <Row>
-          {this.state.products.map((el) => {
-            return (
-              <Col md={2} key={el.id}>
-                <Link to={"/prodotto/" + el.id}>
-                  <Card>
-                    <Card.Img variant="top" src={el.thumbnail} />
-                    <Card.Body>
-                      <Card.Title>{el.title}</Card.Title>
-                      <Card.Text>{el.brand}</Card.Text>
-                      <Button variant="primary">Vedi dettagli</Button>
-                    </Card.Body>
-                  </Card>
-                </Link>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container className="mt-4">
+      <Row>
+        {products.map((el) => {
+          return (
+            <Col md={2} key={el.id}>
+              <Link to={"/prodotto/" + el.id}>
+                <Card>
+                  <Card.Img variant="top" src={el.thumbnail} />
+                  <Card.Body>
+                    <Card.Title>{el.title}</Card.Title>
+                    <Card.Text>{el.brand}</Card.Text>
+                    <Button variant="primary">Vedi dettagli</Button>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+};
 
 export default Products;
